c78a23f905d2b7522cbe90ef7e99c0547db9116b
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-07-27 20:45:56 -0400

    Restructured files in src/components folder to be clean

---
+++ client/src/utils.js/mutations.js
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+//Need to add the differentiation between nanny and user with boolen value still
+export const ADD_USER = gql`
+  mutation addUser($firstname: String!, $lastname: String!, $phonenumber: String!, $username: String!, $email: String!, $password: String!) {
+    addUser(firstname: $firstname, lastname: $lastname, phonenumber: $phonenumber, username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
