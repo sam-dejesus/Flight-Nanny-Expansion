7a472b2275c9ddf02f7d416d53c98a2317a99e48
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-07-31 21:17:30 -0400

    Fixed bug for phonenumber in Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -78,8 +78,8 @@ const Signup = () => {
                   <input
                     className="form-input"
                     placeholder="Your phone number"
-                    name="username"
-                    type="text"
+                    name="phonenumber"
+                    type="number"
                     value={formState.phonenumber}
                     onChange={handleChange}
                   />
